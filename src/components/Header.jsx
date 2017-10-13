d984cce9c0884b27dab5a0b601e3d513a14f1f82
andriih <andrii.hnatyshyn@gmail.com>
2017-10-13 13:27:33 +0300

    Stopwatch added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Stats from './stats'
-
+import Stopwatch from './stopwatch'
 function Header(props){
 	return(
 		
 		<header>
 		<Stats todos={props.todos}/>
 			<h1>{props.title}</h1>
+		<Stopwatch />
 		</header>
+
 	)
 }
 
